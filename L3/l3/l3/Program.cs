f9b3a2ca96484732acfc1788fd108f8f62270507
Maryna Sivachenko <masi23247@gmail.com>
2020-11-02 00:48:35 +0200

    L3

---
+++ L3/l3/l3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace l3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            IIG.PasswordHashingUtils.PasswordHasher.Init(null, 0);
+            IIG.PasswordHashingUtils.PasswordHasher.GetHash("1234");
+                        Console.ReadKey();
+
+        }
+
+        
+
+    }
+}
